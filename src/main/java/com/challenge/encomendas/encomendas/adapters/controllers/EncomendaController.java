50e87d2d84b7b819281f5634495decbc1d6667ce
Jnpacheco-125 <rm350127@fiap.com.br>
2025-04-27 21:36:43 -0300

    Endpoint cadastra encomenda feito

--- src/main/java/com/challenge/encomendas/encomendas/adapters/controllers/EncomendaController.java
+++ src/main/java/com/challenge/encomendas/encomendas/adapters/controllers/EncomendaController.java
@@ -1,12 +1,25 @@
 package com.challenge.encomendas.encomendas.adapters.controllers;
 
+import com.challenge.encomendas.encomendas.adapters.controllers.dto.encomendas.EncomendaRequestDTO;
+import com.challenge.encomendas.encomendas.adapters.controllers.dto.encomendas.EncomendaResponseDTO;
 import com.challenge.encomendas.encomendas.domain.entities.Encomenda;
+import com.challenge.encomendas.encomendas.infrastructure.persistence.mappers.EncomendaMapper;
 import com.challenge.encomendas.encomendas.usecase.cadastro.EncomendaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
+@Slf4j
 @RestController
 @RequestMapping("/encomendas")
 public class EncomendaController {
@@ -18,11 +31,55 @@ public class EncomendaController {
         this.encomendaService = encomendaService;
     }
 
+    @Operation(
+            summary = "Cadastro de encomenda",
+            description = "Registra uma nova encomenda no sistema.",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = @Content(
+                            mediaType = "application/json",
+                            examples = {
+                                    @ExampleObject(
+                                            name = "Exemplo de cadastro de encomenda",
+                                            value = """
+                            {
+                              "nomeDestinatario": "Carlos Souza",
+                              "apartamento": "101",
+                              "descricao": "Caixa grande - Loja X",
+                              "dataRecebimento": "2025-04-27T20:00:00",
+                              "retirada": false,
+                              "funcionarioRecebimento": {
+                                "id": 1,
+                                "nome": "João Porteiro"
+                              },
+                              "moradorDestinatario": {
+                                "id": 10,
+                                "nome": "Carlos Souza"
+                              }
+                            }
+                            """
+                                    )
+                            }
+                    )
+            )
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Encomenda cadastrada com sucesso",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = EncomendaResponseDTO.class))),
+            @ApiResponse(responseCode = "400", description = "Dados inválidos ou erro no cadastro", content = @Content)
+    })
+    @SecurityRequirement(name = "Bearer Auth")
     @PreAuthorize("hasRole('PORTEIRO') or hasRole('ADMIN')")
-    @PostMapping
-    public ResponseEntity<Encomenda> criarEncomenda(@RequestBody Encomenda encomenda) {
-        return ResponseEntity.ok(encomendaService.cadastrarEncomenda(encomenda));
+    @PostMapping("/encomendas")
+    public ResponseEntity<EncomendaResponseDTO> cadastrarEncomenda(@Valid @RequestBody EncomendaRequestDTO requestDTO) {
+        log.info("Cadastro de encomenda solicitado para o morador: {}", requestDTO.moradorDestinatario().nome());
+
+        Encomenda novaEncomenda = encomendaService.cadastrar(requestDTO);
+        EncomendaResponseDTO response = EncomendaMapper.toResponseDTO(novaEncomenda);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
+
 
     @PreAuthorize("hasRole('PORTEIRO')")
     @PutMapping("/{id}/retirada")
--- src/main/java/com/challenge/encomendas/encomendas/domain/entities/Encomenda.java
+++ src/main/java/com/challenge/encomendas/encomendas/domain/entities/Encomenda.java
@@ -4,16 +4,18 @@ import java.time.LocalDateTime;
 
 public class Encomenda {
 
-    private final Long id;
-    private final String nomeDestinatario;
-    private final String apartamento;
-    private final String descricao;
+    private Long id;
+    private String nomeDestinatario;
+    private String apartamento;
+    private String descricao;
     private LocalDateTime dataRecebimento;
     private Boolean retirada;
     private LocalDateTime dataRetirada;
-    private final Funcionario funcionarioRecebimento;
-    private final Morador moradorDestinatario;
+    private Funcionario funcionarioRecebimento;
+    private Morador moradorDestinatario;
 
+    public Encomenda() {
+    }
     public Encomenda(Long id, String nomeDestinatario, String apartamento, String descricao,
                      LocalDateTime dataRecebimento, Boolean retirada, LocalDateTime dataRetirada,
                      Funcionario funcionarioRecebimento, Morador moradorDestinatario) {
@@ -40,7 +42,34 @@ public class Encomenda {
     public Morador getMoradorDestinatario() { return moradorDestinatario; }
 
     // Setters para atributos que podem mudar
-    public void setRetirada(Boolean retirada) { this.retirada = retirada; }
-    public void setDataRetirada(LocalDateTime dataRetirada) { this.dataRetirada = dataRetirada; }
-    public void setDataRecebimento(LocalDateTime dataRecebimento) { this.dataRecebimento = dataRecebimento; }
+    public void setNomeDestinatario(String nomeDestinatario) {
+        this.nomeDestinatario = nomeDestinatario;
+    }
+
+    public void setApartamento(String apartamento) {
+        this.apartamento = apartamento;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setDataRecebimento(LocalDateTime dataRecebimento) {
+        this.dataRecebimento = dataRecebimento;
+    }
+
+    public void setRetirada(Boolean retirada) {
+        this.retirada = retirada;
+    }
+
+    public void setDataRetirada(LocalDateTime dataRetirada) {
+        this.dataRetirada = dataRetirada;
+    }
+    public void setFuncionarioRecebimento(Funcionario funcionarioRecebimento) {
+        this.funcionarioRecebimento = funcionarioRecebimento;
+    }
+
+    public void setMoradorDestinatario(Morador moradorDestinatario) {
+        this.moradorDestinatario = moradorDestinatario;
+    }
 }
